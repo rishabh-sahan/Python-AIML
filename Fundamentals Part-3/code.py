e204ff728da8a9882e615b701cc17497ab22c3c4
Rishabh Jain <rishabhsahan@gmail.com>
2026-03-16 08:44:49 +0530

    Updated Fundamentals Part-3

---
+++ Fundamentals Part-3/code.py
@@ -0,0 +1,58 @@
+# Strings
+# word = "python"
+# print(len(word))
+
+# word1 = "Hello"
+# word2 = "World"
+# print(word1 + " " + word2) # Concatenation
+
+# word = "python"
+# print(word[1]) # Accessing characters
+# for ch in word:
+#     print(ch) # Iterating through characters
+
+# Slicing
+# word = "Rishabh Sahan Jain"
+# print(word[8:13]) # Sahan
+# print(word[14:]) # Jain
+# print(word[:7]) # Rishabh
+
+# Normal Formatting
+# a = 5
+# b = 10
+# sum = a + b
+# print("Sum of {} and {} is {}".format(a, b, sum)) # String formatting
+
+# f-Strings
+# a = 5
+# b = 10
+# print(f"Sum of {a} and {b} is {a + b}") # f-String formatting
+
+# marks = [99, 89, 100, 65, 95, 111.11, "abcd"]
+# marks[2] = 95 # Modifying elements
+# print(marks)
+# print(type(marks)) 
+# print(len(marks))
+# print(marks[2]) # Accessing elements
+# print(marks[1:4]) # Slicing elements in list
+# print(marks[5:])
+
+# nums = [1, 2, 3, 4]
+# nums.append(5) # Adding element to the end of the list
+# print(nums)
+
+# nums.insert(2, 2.5) # Inserting element at a specific index
+# print(nums)
+
+# nums.insert(4, 10) 
+# print(nums)
+
+# nums.sort() # Sorting the list
+# print(nums)
+
+# nums.sort(reverse=True) # Sorting in reverse order
+# print(nums)
+
+# nums.reverse() # Reversing the list
+# print(nums)
+
